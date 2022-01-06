e70715b1ee8b276124a9ea7c316bd421158394c1
Honor <dmitrilay@gmail.com>
2022-01-07 00:35:53 +0600

    Новая функция, замена b на d

--- nums.py
+++ nums.py
@@ -1,16 +1,21 @@
-def plus(a, b):
-    return a + b
+def plus(a, d):
+    return a + d
 
 
-def minus(a, b):
-    return a - b
+def minus(a, d):
+    return a - d
 
 
-def mul(a, b):
-    return a * b
+def mul(a, d):
+    return a * d
+
+
+def mul2(a, b, c):
+    return a * b * c
 
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(2, 2))
     print(mul(3, 3))
+    print(mul2(3, 3, 2))
